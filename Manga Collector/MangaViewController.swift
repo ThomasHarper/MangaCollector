09fa2e643ec4e654f606ad25906fe6d18f64d5c9
Thomas Harper <thomasbentkowski@gmail.com>
2016-11-04 14:38:12 +0100

    Adding UIImagePickerController and asking the user to access his photo (info.plist)

--- Manga Collector/MangaViewController.swift
+++ Manga Collector/MangaViewController.swift
@@ -8,22 +8,28 @@
 
 import UIKit
 
-class MangaViewController: UIViewController {
+class MangaViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
-    
+    /////////////////// OUTLETS ////////////////////
     @IBOutlet weak var mangaImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    ///////////////// PROPERTIES ///////////////////
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
     
     @IBAction func cameraTapped(_ sender: AnyObject) {
     }
     
     @IBAction func photosTapped(_ sender: AnyObject) {
+        // Telling the UIImagePickerController that we need to use the photo library
+        imagePicker.sourceType = .photoLibrary
+        // Then show this UIImagePickerController to the user
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func addTapped(_ sender: AnyObject) {
